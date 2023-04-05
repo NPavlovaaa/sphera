da23b9b716519510d7f39b0b7a75cffc013b82b1
NPavlovaaa <n.pavlova.web@gmail.com>
2023-04-05 22:38:56 +0300

    fixed css properties and weight_selection in the ProductList component

--- src/components/productList/ProductList.js
+++ src/components/productList/ProductList.js
@@ -91,15 +91,13 @@ const ProductList = () => {
             })
 
             const weights_render = weightSelection.map(weight_selection => {
-                let current_weight = []
+                let current_weight = [];
                 if (weight_selection.product === product.product_id){
                     weights.map(item => {
                         if (item.weight_id === weight_selection.weight){
+                            current_weight.push(weight_selection.weight_selection_id)
                             current_weight.push(item.weight)
                             current_weight.push(weight_selection.price)
-                        }
-                        else{
-                            return null
                         }
                         return current_weight
                 })}
@@ -108,12 +106,12 @@ const ProductList = () => {
 
             return (
                 <li
-                    className="flex mr-10"
+                    className=""
                     tabIndex={0}
                     ref={el => itemRefs.current[i] = el}
                     key={product.product_id}
                 >
-                    <div className="bg-lightGray w-1/3 rounded-lg px-8 py-4 pb-5">
+                    <div className="bg-lightGray rounded-lg px-8 py-4 pb-5">
                         <div className="mb-5 flex flex-row text-xs">
                             <p className="text-mainGray l-0">{roasting}</p>
                             <p className="text-mainGray ml-3">{processing}</p>
@@ -122,12 +120,14 @@ const ProductList = () => {
                             <p className="text-xl font-medium mb-3">{product.product_name}</p>
                             <img src={bobs250} width="200" alt="Картинка товара" />
                             <p className="text-md text-mainGray mt-3">{variety}</p>
-                            <p className="text-sm text-mainGray mt-2">{product.taste}</p>
+                            <p className="text-sm w-full left-0 text-mainGray mt-3 h-9">{product.taste}</p>
                             <div className="flex flex-col text-sm text-mainGray w-full mt-5">
                                 <div className="flex justify-between">
                                     <div className="flex flex-col items-center mb-2">
                                         <p className="flex mb-0.5">Кислотность</p>
-                                        {renderParams(acidity)}
+                                        <div className="flex">
+                                            {renderParams(acidity)}
+                                        </div>
                                     </div>
                                     <div className="flex flex-col items-center">
                                         <p className="flex mb-0.5">Плотность</p>
@@ -147,22 +147,25 @@ const ProductList = () => {
                             </div>
                             <div className="flex flex-row justify-between w-full mt-5">
                                 {weights_render.map(item => {
-                                    return (
-                                        <div>
-                                            <input class="peer sr-only" type="radio" value={item[0]} name="weight" id={item[0]} checked/>
-                                            <label class="text-xs text-mainGray flex justify-center cursor-pointer rounded-lg border-b-mainOrange-600 mainOrange py-1 px-2 focus:outline-none peer-checked:border-transparent peer-checked:ring-1 peer-checked:ring-mainOrange-600 transition-all duration-500 ease-in-out" for={item[0]}>
-                                                {item[0]}
-                                            </label>
-                                            <div class="mt-3 text-2xl justify-center flex transition-all duration-500 ease-in-out translate-x-40 opacity-0 invisible peer-checked:opacity-100 peer-checked:visible peer-checked:translate-x-1">
-                                                {item[1]} р
+                                    if (item[1] !== undefined) {
+                                        return (
+                                            <div>
+                                                <input class="peer sr-only" type="radio" value={item[0]} name="weight" id={item[0]} checked/>
+                                                <label class="text-xs text-mainGray flex justify-center cursor-pointer rounded-lg border-b-mainOrange-600 mainOrange py-1 px-2 focus:outline-none peer-checked:border-transparent peer-checked:ring-1 peer-checked:ring-mainOrange-600 transition-all duration-500 ease-in-out" for={item[0]}>
+                                                    {item[1]}
+                                                </label>
+                                                <div class="mt-3 text-xl justify-center flex transition-all duration-500 ease-in-out translate-x-40 opacity-0 invisible peer-checked:opacity-100 peer-checked:visible peer-checked:translate-x-1">
+                                                    {item[2]} р
+                                                </div>
                                             </div>
-                                        </div>
-                                    )
+                                        )
+                                    }else return null
+
                                 })}
                             </div>
                         </div>
                         <div className="flex justify-end mt-5">
-                            <button type="submit" className="bg-mainOrange-600 shadow-xl rounded-lg px-5 py-2">В корзину</button>
+                            <button type="submit" className="bg-mainOrange-600 rounded-2xl px-5 py-2">В корзину</button>
                         </div>
                     </div>
                 </li>
@@ -170,7 +173,7 @@ const ProductList = () => {
         })
 
         return (
-            <ul className="w-full">
+            <ul className="grid grid-cols-3 gap-10 w-full">
                 {items}
             </ul>
         )
